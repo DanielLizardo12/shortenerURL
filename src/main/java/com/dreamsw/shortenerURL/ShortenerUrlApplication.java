8825eaa2d6d8b915e91ff7dc3138fd5d39ffe10e
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-06-02 20:05:21 -0600

    initial commit

---
+++ src/main/java/com/dreamsw/shortenerURL/ShortenerUrlApplication.java
@@ -0,0 +1,13 @@
+package com.dreamsw.shortenerURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenerUrlApplication.class, args);
+	}
+
+}
