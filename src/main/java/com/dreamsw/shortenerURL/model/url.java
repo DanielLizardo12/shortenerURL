376d8189733c773a707155c9e2652cdcf67281e5
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-06-07 15:27:27 -0600

    added test model

---
+++ src/main/java/com/dreamsw/shortenerURL/model/url.java
@@ -0,0 +1,16 @@
+package com.dreamsw.shortenerURL.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class url {
+    
+    private Long id;
+    private String url;
+}
